c806b03b85d928c834df54285a43d4bcf4f79bac
rigole <foplacide@gmail.com>
2021-07-12 10:43:34 +0100

    opening and closing the modal by clicking outside

--- script.js
+++ script.js
@@ -15,3 +15,4 @@ function showModal() {
 // Modal Event Listeners
 modalShow.addEventListener('click',showModal);
 modalClose.addEventListener('click',() => modal.classList.remove('show-modal'));
+window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove( 'show-modal'): false));
