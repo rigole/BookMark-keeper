7577f7ce2d7fe21999b649dda2bc74b61fe63276
rigole <foplacide@gmail.com>
2021-07-12 16:59:31 +0100

    validating form and url with Regex

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
+let bookmarks = [];
+
 // Show Modal, Focus on Input
 function showModal() {
     modal.classList.add('show-modal');
@@ -16,3 +18,44 @@ function showModal() {
 modalShow.addEventListener('click',showModal);
 modalClose.addEventListener('click',() => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove( 'show-modal'): false));
+
+// Validate Form
+function validate(nameValue, urlValue) {
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+
+    if (!nameValue || !urlValue){
+        alert('Please submit values for both fields');
+        return false;
+    }
+    if (!urlValue.match(regex)){
+        alert('Please provide a valid address');
+        return false;
+    }
+    return true;
+}
+
+function storeBookmark(e) {
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if (!urlValue.includes('http://','https://')){
+        urlValue = `https://${urlValue}`;
+    }
+    console.log(nameValue, urlValue);
+    if(!validate(nameValue, urlValue)){
+        return false;
+    }
+
+    const bookmark = {
+        name: nameValue,
+        url: urlValue,
+    };
+    bookmarks.push(bookmark);
+    console.log(bookmarks);
+    bookmarkForm.reset();
+    websiteNameEl.focus();
+}
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
